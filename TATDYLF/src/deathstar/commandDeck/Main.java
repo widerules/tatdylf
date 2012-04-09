93f4599a4bad5f22e8de0eec0b877a16c5e3c30d
forgetfultitan <forgetfultitan@gmail.com>
2012-04-09 21:35:12 +0000

    Added the shell of what is going to be there

---
+++ TATDYLF/src/deathstar/commandDeck/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package deathstar.commandDeck;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Main extends JFrame implements ActionListener {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, Android!");
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
