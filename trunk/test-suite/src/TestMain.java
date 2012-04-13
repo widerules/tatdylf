3608335c4ac48400ed77f7d1ef1ed75a13b04fa7
neil.deshpa@gmail.com <neil.deshpa@gmail.com@1524be4f-b4e9-4e6b-3925-93f27282685a>
2012-04-13 02:03:39 +0000

    Test project

---
+++ trunk/test-suite/src/TestMain.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
+public class TestMain {
+
+	public static void main(String[] args) throws IOException{
+		Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 8080);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		bw.write("VIB");
+		bw.close();
+		socket.close();
+	}
+}
+
