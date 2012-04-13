c71fef37d9d96e28514e9b430973c56cfbc300c2
koala0106 <koala0106@gmail.com>
2012-04-13 02:32:39 +0000

    updated to port 8081

--- test-suite/src/TestMain.java
+++ test-suite/src/TestMain.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 public class TestMain {
 
 	public static void main(String[] args) throws IOException{
-		Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 8080);
+		Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 8081);
 		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 		bw.write("VIB");
