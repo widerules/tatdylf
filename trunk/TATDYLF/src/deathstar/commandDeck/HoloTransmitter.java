3d593a941052043c719067ac011ed8fa5a13c8e5
koala0106@gmail.com <koala0106@gmail.com@1524be4f-b4e9-4e6b-3925-93f27282685a>
2012-04-15 02:17:34 +0000

    added mail class and test mailing function

---
+++ trunk/TATDYLF/src/deathstar/commandDeck/HoloTransmitter.java
@@ -0,0 +1,51 @@
+package deathstar.commandDeck;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class HoloTransmitter {
+	
+	public HoloTransmitter() {
+	}
+
+	public static boolean sendSMS() throws MessagingException {
+		boolean debug = false;
+
+	    //Set the host smtp address
+	    Properties props = new Properties();
+	    props.put("mail.smtp.host", "localhost");
+	    
+
+	    // create some properties and get the default Session
+	    Session session = Session.getDefaultInstance(props, null);
+	    session.setDebug(debug);
+
+	    // create a message
+	    Message msg = new MimeMessage(session);
+
+	    // set the from and to address
+	    InternetAddress addressFrom = new InternetAddress("koala0106@gmail.com");
+	    msg.setFrom(addressFrom);
+
+	    InternetAddress[] addressTo = new InternetAddress[2]; 
+        addressTo[0] = new InternetAddress("ndw2114@columbia.edu");
+        addressTo[1] = new InternetAddress("nad2135@columbia.edu");
+	    msg.setRecipients(Message.RecipientType.TO, addressTo);
+	   
+
+	    // Optional : You can also set your custom headers in the Email if you Want
+	    msg.addHeader("MyHeaderName", "myHeaderValue");
+
+	    // Setting the Subject and Content Type
+	    msg.setSubject("Email from Java");
+	    msg.setContent("I really hope this works", "text/plain");
+	    Transport.send(msg);
+		return true;
+	    }
+}
