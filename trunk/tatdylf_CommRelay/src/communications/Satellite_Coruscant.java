5a9568f7be881b703cffafd2b024b8411aa065b4
koala0106@gmail.com <koala0106@gmail.com@1524be4f-b4e9-4e6b-3925-93f27282685a>
2012-04-17 19:22:59 +0000

    Created base Satellite class that Satellite Deathstar and Coruscant inherit from

--- trunk/tatdylf_CommRelay/src/communications/Satellite_Coruscant.java
+++ trunk/tatdylf_CommRelay/src/communications/Satellite_Coruscant.java
@@ -10,104 +10,14 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Satellite_Coruscant extends Thread {
-	
-	boolean keepListening, closed;
-	int port;
-	
+public class Satellite_Coruscant extends Satellite {
 	public Satellite_Coruscant(){
+		super();
 		port = 61244;
 	}
 	
 	public Satellite_Coruscant(int port){
+		super();
 		this.port = port;
 	}
-	
-	public Socket talkToSelf(){
-		Socket socket = null;
-		try {
-			socket = new Socket(InetAddress.getByName("127.0.0.1"), port);
-		} catch (Exception e) {
-			return null;
-		}
-		return socket;
-	}
-	
-	public void stopTalking(Socket s){
-		try {
-			s.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public boolean stopListening(Socket socket) {
-		try {
-			keepListening = false;
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			out.println("Bye.");
-			out.close();
-		} catch (Exception e) {
-			keepListening = true;
-			return false;
-		}
-		return closed;
-	}
-	
-	public void run() {
-		try {
-			listen(port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public String listen(int socket) throws IOException{
-		
-		keepListening = true;
-		closed = false;
-		
-		ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(socket);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 51244.");
-            System.exit(1);
-        }
-
-		while (keepListening) {
-			Socket clientSocket = null;
-			try {
-				clientSocket = serverSocket.accept();
-				System.out.println("Connected");
-			} catch (IOException e) {
-				System.err.println("Accept failed.");
-				System.exit(1);
-			}
-
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
-					true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream()));
-			String inputLine;
-
-			while ((inputLine = in.readLine()) != null) {
-				if (inputLine.equals("Bye.")){
-					out.println("Fine, I don't need you!  Jsut leave!");
-					break;
-				}
-				handleMessage(inputLine, out);
-			}
-		}
-		closed = true;
-		return null;
-	}
-	
-	private void handleMessage(String s, PrintWriter out){
-		System.out.println(s);
-		Relay.sendMessage(out, "Message Received");
-	}
-	
 }
--- trunk/tatdylf_CommRelay/src/communications/Satellite_Deathstar.java
+++ trunk/tatdylf_CommRelay/src/communications/Satellite_Deathstar.java
@@ -11,106 +11,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Satellite_Deathstar extends Thread {
-	
-	boolean keepListening, closed;
-	int port;
-	
+public class Satellite_Deathstar extends Satellite {
 	public Satellite_Deathstar(){
+		super();
 		port = 61244;
 	}
 	
 	public Satellite_Deathstar(int port){
+		super();
 		this.port = port;
 	}
-	
-	public Socket talkToSelf(){
-		Socket socket = null;
-		try {
-			socket = new Socket(InetAddress.getByName("127.0.0.1"), port);
-		} catch (Exception e) {
-			return null;
-		}
-		return socket;
-	}
-	
-	public void stopTalking(Socket s){
-		try {
-			s.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public boolean stopListening(Socket socket) {
-		try {
-			keepListening = false;
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			out.println("Bye.");
-			out.close();
-		} catch (Exception e) {
-			keepListening = true;
-			return false;
-		}
-		return closed;
-	}
 
-	public void run() {
-		try {
-			listen(port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public String listen(int socket) throws IOException{
-		
-		keepListening = true;
-		closed = false;
-		
-		ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(socket);
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 51245.");
-            System.exit(1);
-        }
-        
-		while (keepListening) {
-			Socket clientSocket = null;
-			try {
-				clientSocket = serverSocket.accept();
-				System.out.println("Connected");
-			} catch (IOException e) {
-				System.err.println("Accept failed.");
-				System.exit(1);
-			}
-
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
-					true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream()));
-			String inputLine;
-
-			out.println("I see you!");
-
-			while ((inputLine = in.readLine()) != null) {
-				if (inputLine.equals("Bye.")){
-					out.println("Fine, I don't need you!  Just leave!");
-					break;
-				}
-				handleMessage(inputLine, out);
-			}
-		}
-		closed = true;
-		return null;
-	}
-	
-	private void handleMessage(String s, PrintWriter out){
-		System.out.println(s);
-		Relay.sendMessage(out, "Message Received");
-	}
-	
 }
