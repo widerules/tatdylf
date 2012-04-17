66c0fa83e7487df52af3efd58b48c113b7f1b463
koala0106@gmail.com <koala0106@gmail.com@1524be4f-b4e9-4e6b-3925-93f27282685a>
2012-04-17 20:27:16 +0000

    added a test class to test satellite communication

---
+++ trunk/test-suite/src/TestSatellite.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
+public class TestSatellite {
+	public static void main(String args[]) {
+		Socket socket = null;
+		String input;
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			while (!(input = stdIn.readLine()).equals("End")) {
+				socket = new Socket(InetAddress.getByName("127.0.0.1"), 61244);
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out.println(input);
+				input = in.readLine();
+				System.out.println("received: " + input);
+				in.close();
+				out.close();
+				socket.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+}
