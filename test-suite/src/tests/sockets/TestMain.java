279efaf47ecc9d1046564a88bd6a286887eb040b
neil.deshpa <neil.deshpa@gmail.com>
2012-04-18 23:56:39 +0000

    Moved the test classes to appropriate packages and added tests for encryption

---
+++ test-suite/src/tests/sockets/TestMain.java
@@ -0,0 +1,27 @@
+package tests.sockets;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+
+/**
+ * @author neil
+ *
+ */
+public class TestMain {
+
+	public static void main(String[] args) throws IOException{
+//		Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 8081);
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+//		bw.write("Go Dispatcher!");
+//		bw.close();
+//		socket.close();
+		
+		
+		
+	}
+}
+
