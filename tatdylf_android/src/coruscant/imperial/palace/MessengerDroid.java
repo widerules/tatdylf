ea0cb83f0a046f07bb0473eda2da4d0f2e3f2d10
koala0106 <koala0106@gmail.com>
2012-04-21 00:07:37 +0000

    Updated MessengerDroid

--- tatdylf_android/src/coruscant/imperial/palace/MessengerDroid.java
+++ tatdylf_android/src/coruscant/imperial/palace/MessengerDroid.java
@@ -1,7 +1,9 @@
 package coruscant.imperial.palace;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import android.content.res.Resources;
 import android.util.Log;
@@ -10,10 +12,12 @@ import comm.messaging.Command;
 import comm.messaging.Message;
 import comm.messaging.SecureChannel;
 import comm.messaging.SimplMessage;
+
 import coruscant.imperial.palace.comm.RSAUtilImpl;
 
 public class MessengerDroid extends Thread {
 	
+	String computerIP;
 	Dispatcher dispatch;
 	boolean keepListening;
 	Resources resources;
@@ -23,49 +27,44 @@ public class MessengerDroid extends Thread {
 		dispatch = new Dispatcher();
 		resources = res;
 		channel = new SecureChannel(new RSAUtilImpl(res));
+		computerIP = "69.203.17.33";
 	}
 	
+	private Socket openOutboundSocket() throws UnknownHostException, IOException {
+		Socket s = new Socket(computerIP, 61245);
+		return s;
+	}
 
 	public void run() {
-		/* Test inbound connection */
 		keepListening = true;
 		ServerSocket server;
 		try {
+			/* outbound communication */
+/*			Log.d("MessengerDroid", "Trying to create client socket");
+			Socket clientSocket = openOutboundSocket();
+			Message msg_out = new SimplMessage();
+			msg_out.addParam("COMM", "Hello");
+			channel.serialize(msg_out, clientSocket);*/
 			
-			Log.d("MessengerDroid", "Trying to create client socket");
-			Socket socket = new Socket("10.0.2.15", 61246);
-			Message msg = new SimplMessage();
-			msg.addParam("COMM", "Hello");
-			channel.serialize(msg, socket);
-			
-//			Log.d("MessengerDroid", "Trying to create server socket");
-//			server = new ServerSocket(61246);
-//			while(keepListening) {
-//				Log.d("MessengerDroid", "Now waiting for connections on " + server.getLocalPort());
-//
-//				Socket socket = server.accept();
-//				
-//				//BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//				//String cmd = reader.readLine();
-//				
-//				//Message msg = SecureChannel.deSerializeMsg(socket);
-//				Message msg = null;
-//				Log.d("MessengerDroid","Received msg:" + msg.prettyPrint());
-//				if(msg.getCmd() == Command.EXIT) {
-//					keepListening = false;
-//				}
-//				else {
-//					//PrintWriter out = new PrintWriter(socket.getOutputStream(),	true);
-//					//BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
-//					dispatch.handleCommand(msg);
-//					//Message res = new SimplMessage();
-//					//res.addParam("RESPONSE", "Message Dispatched");
-//					//SecureChannel.serializeMsg(res, out);
-//					//out.close();
-//					//socket.close();
-//				}
-//			}
-//			Log.d("MessengerDroid", "exiting");
+			Log.d("MessengerDroid", "Trying to create server socket");
+			server = new ServerSocket(61244);
+			while(keepListening) {
+				Log.d("MessengerDroid", "Now waiting for connections on " + server.getLocalPort());
+				Socket socket = server.accept();
+				Message msg_in = channel.deSerialize(socket);
+				Log.d("MessengerDroid","Received msg:" + msg_in.prettyPrint());
+				if(msg_in.getCmd() == Command.EXIT) {
+					keepListening = false;
+				}
+				else {
+					dispatch.handleCommand(msg_in);
+					Socket clientSocket = openOutboundSocket();
+					Message res = new SimplMessage();
+					res.addParam("RESPONSE", "Message Dispatched");
+					channel.serialize(res, clientSocket);
+				}
+			}
+			Log.d("MessengerDroid", "exiting");
 		} catch (Exception e) {
 			Log.e("MessengerDroid", "Error in IO", e);
 		}
