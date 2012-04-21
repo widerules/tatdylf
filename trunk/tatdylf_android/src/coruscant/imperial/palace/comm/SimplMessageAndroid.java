d121463997eaeb0562b09c6b7ab228119566aa6e
koala0106@gmail.com <koala0106@gmail.com@1524be4f-b4e9-4e6b-3925-93f27282685a>
2012-04-21 02:21:30 +0000

    fixed JSON lookup command bug

--- trunk/tatdylf_android/src/coruscant/imperial/palace/comm/SimplMessageAndroid.java
+++ trunk/tatdylf_android/src/coruscant/imperial/palace/comm/SimplMessageAndroid.java
@@ -5,13 +5,14 @@ import org.json.JSONObject;
 
 import comm.messaging.Command;
 import comm.messaging.Message;
+import comm.messaging.Param;
 import comm.messaging.SimplMessage;
 
 public class SimplMessageAndroid extends SimplMessage {
 
 	@Override
 	public Command getCmd() throws JSONException {
-		return Command.values()[(Integer) json.get(CMD)];
+		return Command.values()[(Integer) json.get(Param.COMMAND)];
 	}
 
 	@Override
