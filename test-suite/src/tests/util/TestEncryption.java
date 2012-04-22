9c95a1813f276aef3808e6f475689e96d7325250
neil.deshpa <neil.deshpa@gmail.com>
2012-04-22 02:27:00 +0000

    Removed compile errors from test-suite

--- test-suite/src/tests/util/TestEncryption.java
+++ test-suite/src/tests/util/TestEncryption.java
@@ -10,16 +10,16 @@ public class TestEncryption {
 	 */
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		RSAUtilImpl.initialize("/home/neil/");
-		System.out.println("RSAUtil initialized");
-		
-		StringBuilder sb = new StringBuilder();
-		
-		byte[] cipher = RSAUtilImpl.encrypt("Hello World".getBytes());
-		System.out.println(new String(cipher));
-		byte[] data = RSAUtilImpl.decrypt(cipher);
-		String str = new String(data);
-		System.out.println(str);
+//		RSAUtilImpl.initialize("/home/neil/");
+//		System.out.println("RSAUtil initialized");
+//		
+//		StringBuilder sb = new StringBuilder();
+//		
+//		byte[] cipher = RSAUtilImpl.encrypt("Hello World".getBytes());
+//		System.out.println(new String(cipher));
+//		byte[] data = RSAUtilImpl.decrypt(cipher);
+//		String str = new String(data);
+//		System.out.println(str);
 		
 	}
 
