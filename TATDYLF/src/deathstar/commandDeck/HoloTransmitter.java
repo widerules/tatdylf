c186d54f2d3ff52edf15a3273b1aa1848e08514a
forgetfultitan <forgetfultitan@gmail.com>
2012-04-23 21:36:50 +0000

    Added asynchronous support for the UI

--- TATDYLF/src/deathstar/commandDeck/HoloTransmitter.java
+++ TATDYLF/src/deathstar/commandDeck/HoloTransmitter.java
@@ -3,7 +3,6 @@ package deathstar.commandDeck;
 import java.util.Properties;
 
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
