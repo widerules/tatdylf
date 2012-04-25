610b5fcebd6dce77f8b68510d433f292a44e2325
neil.deshpa <neil.deshpa@gmail.com>
2012-04-25 02:31:39 +0000

    Corrected port numbers

--- TATDYLF/src/deathstar/commandDeck/Constant.java
+++ TATDYLF/src/deathstar/commandDeck/Constant.java
@@ -3,6 +3,7 @@ package deathstar.commandDeck;
 public class Constant {
 	
 	public static final String COMM_RELAY = "128.59.13.32";
-	public static final int COMM_RELAY_PORT = 61246;
+	public static final int COMM_RELAY_INIT_PORT = 61246;
+	public static final int COMM_RELAY_PORT = 61244;
 
 }
