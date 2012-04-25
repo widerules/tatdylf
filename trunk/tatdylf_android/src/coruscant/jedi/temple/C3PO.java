0996599fa77db4c8badf0474ab132bcbe6dfc33d
neil.deshpa@gmail.com <neil.deshpa@gmail.com@1524be4f-b4e9-4e6b-3925-93f27282685a>
2012-04-25 03:01:00 +0000

    Hopefully, should now listen for volume changes

--- trunk/tatdylf_android/src/coruscant/jedi/temple/C3PO.java
+++ trunk/tatdylf_android/src/coruscant/jedi/temple/C3PO.java
@@ -4,10 +4,10 @@ import java.net.Socket;
 
 import org.json.JSONException;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
+import android.database.ContentObserver;
 import android.media.AudioManager;
+import android.os.Handler;
 import android.util.Log;
 
 import comm.messaging.Param;
@@ -18,15 +18,33 @@ import coruscant.imperial.palace.comm.RSAUtilImpl;
 import coruscant.imperial.palace.comm.SecureChannelAndroid;
 import coruscant.imperial.palace.comm.SimplMessageAndroid;
 
-public class C3PO extends BroadcastReceiver {
+public class C3PO extends ContentObserver {
+	
+	private Context context = null;
+	
+	public void setCtx(Context ctx) {
+		this.context = ctx;
+	}
+
+	public C3PO(Handler handler) {
+		super(handler);
+
+	}
+
 	private AudioManager audioManager;
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		Log.d("C3PO", "Received media button change!");
+	public void onChange(boolean selfChange) {
+		Log.d("C3PO", "Received settings change!");
+		
+		super.onChange(selfChange);
+	    Log.d("C3PO", "Settings change detected");
+		
 		audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 		int vol = audioManager.getStreamVolume(AudioManager.STREAM_RING);
 		int max_vol = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+		
+		
 		SimplMessageAndroid msg_out = new SimplMessageAndroid();
 		try {
 			msg_out.addParam(Param.MSGID, -1);
@@ -36,7 +54,8 @@ public class C3PO extends BroadcastReceiver {
 			e.printStackTrace();
 		}
 		try {
-			Socket s = new Socket(context.getString(R.string.comm_relay_ip), Integer.parseInt(context.getString(R.string.comm_relay_outbound)));
+			Socket s = new Socket(context.getString(R.string.comm_relay_ip),
+					Integer.parseInt(context.getString(R.string.comm_relay_outbound)));
 			SecureChannel channel = new SecureChannelAndroid(new RSAUtilImpl(context));
 			channel.serialize(msg_out, s);
 			Log.d("C3PO", "Sent notification of change");
