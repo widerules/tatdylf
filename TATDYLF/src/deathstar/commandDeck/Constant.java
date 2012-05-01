435342fbaa74156969ba097668a78a7fd948dd40
forgetfultitan <forgetfultitan@gmail.com>
2012-05-01 21:44:52 +0000

    key updates

--- TATDYLF/src/deathstar/commandDeck/Constant.java
+++ TATDYLF/src/deathstar/commandDeck/Constant.java
@@ -2,7 +2,7 @@ package deathstar.commandDeck;
 
 public class Constant {
 	
-	public static final String COMM_RELAY = "128.59.18.103";
+	public static final String COMM_RELAY = "160.39.163.83";
 	public static final int COMM_RELAY_INIT_PORT = 61246;
 	public static final int COMM_RELAY_PORT = 61244;
 
